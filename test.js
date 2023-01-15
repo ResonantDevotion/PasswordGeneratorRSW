aae6c4cf681a423255cdfb82ca76085e1955cf22
Rebecca Wood <rebeccaseverowood@gmail.com>
2023-01-16 07:37:33 +1300

    Added pseudocode and script. Need to redo pseudo to reflect changes and add readme

---
+++ test.js
@@ -0,0 +1,14 @@
+let letter = ["a", "b", "c", "d", "e", "f"]; //6
+
+
+//For Loop can execute a block of code a number of times.
+//   start      // stop (as long as ...)         //pace
+for (let i = letter.length-1; i > 4; i--) {
+    console.log(letter[i]);
+}
+
+
+// While loop can execute as long as a condition is true
+while
+
+
